9122c646b41d793fe3da7146d025a0ca9d20b40e
JOLOLACK <121728988+JOLOLACK@users.noreply.github.com>
2024-07-31 20:17:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const API_KEY = 'api_key=ccce8287f51df54af80b83283695c531'
 const url = 'https://api.themoviedb.org/3';
 const API_URL = url + '/discover/movie?sort_by=popularity.desc&'
+
 const options = {
   method: 'GET',
   headers: {
@@ -9,7 +10,8 @@ const options = {
 
   }
 };
-const accountId = 'YOUR TMDB ACCOUNT'
+
+//const accountId = '21393966'
 
 let totalHour = 0
 let isUpdatingBackground = false;
@@ -17,6 +19,8 @@ let buttonAccountClick = false
 
 document.addEventListener('DOMContentLoaded', () => {
   const creditsBTN = document.getElementById('credits')
+  const accountIdUser= document.getElementById('accountIdUserTMDB')
+  const accountIdINP = document.getElementById('accountIdInput')
   const inputField = document.getElementById('inputfield');
   const dropdown = document.getElementById('dropdown');
   const url = 'https://api.themoviedb.org/3';
@@ -25,9 +29,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const accountCalcBtn = document.getElementById('accountcalculatorBtn')
 
   const minutesMediaEL = document.getElementById('minutesMedia')
-      const hourMediaEL = document.getElementById('hourMedia')
-      const dayMediaEL = document.getElementById('dayMedia')
-  
+  const hourMediaEL = document.getElementById('hourMedia')
+  const dayMediaEL = document.getElementById('dayMedia')
+
 
   inputField.addEventListener('input', async (event) => {
     const query = event.target.value;
@@ -40,6 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
   });
+  
 
   async function fetchDataApi(query) {
     try {
@@ -61,6 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < data.length; i++) {
 
       let element = data[i];
+      console.log(element)
       if (element.poster_path) {
 
         let dropdownItem = document.createElement('div');
@@ -92,7 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
           console.log(runTime)
           addTime(runTime)
 
-          saveDisplay(element.poster_path, element.id, nameMedia, runTime, element.poster_path)
+          saveDisplay(element.poster_path, element.id, nameMedia, runTime, element.backdrop_path)
           dropdown.style.display = 'none';
           inputField.value = ''
         });
@@ -101,15 +107,21 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function updateBackgrounds(url) {
-    const newImageUrl = url;
+    const newImageUrl = `https://image.tmdb.org/t/p/w500${url}`;
     const elements = document.querySelector('.background');
 
     if (!isUpdatingBackground) {
-      isUpdatingBackground = true;
-      setTimeout(() => {
-        elements.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${newImageUrl})`;
-        isUpdatingBackground = false; // Reset the flag after 5 seconds
-      }, 2000);
+      const img = new Image()
+      img.src = newImageUrl
+      img.onload = () =>{
+
+        isUpdatingBackground = true;
+        setTimeout(() => {
+          elements.style.backgroundImage = `url(${newImageUrl})`;
+          elements.style.opacity = '0.3'
+          isUpdatingBackground = false; // Reset the flag after 5 seconds
+        }, 1000);
+      }
     }
   }
 
@@ -145,13 +157,105 @@ document.addEventListener('DOMContentLoaded', () => {
   async function timerCalculator(mediaType, id) {
     let quary = mediaType == "tv" ? `/tv/${id}` : `/movie/${id}`
     let apiResult = await fetchDataApi(quary)
+    let runTime = 0
 
-    let timeMin = mediaType == "tv" ? apiResult.number_of_episodes * apiResult.last_episode_to_air.runtime : apiResult.runtime
+
+    if (mediaType == "tv") {
+      let newQuary = `/tv/${id}/season/${1}`
+
+      runTime = (await fetchDataApi(newQuary)).episodes[1].runtime
+
+
+    }
+    let timeMin = mediaType == "tv" ? apiResult.number_of_episodes * runTime : apiResult.runtime
     let tunredHours = timeMin / 60
 
     return tunredHours
 
   }
+
+
+  async function accountIdData(page, mediaType,userId) {
+    console.log(`/account/${userId}/rated/${mediaType}?page=${page}&sort_by=created_at.asc`)
+    return await fetchDataApi(`/account/${userId}/rated/${mediaType}?page=${page}&sort_by=created_at.asc`)
+
+  }
+
+  accountCalcBtn.addEventListener('click', async () => {
+    console.log(accountIdINP.value)
+    if (accountIdINP.value) {
+
+      buttonAccountClick = true
+      let mediaType = "tv"
+
+      for (let i = 0; i < 2; i++) {
+
+        let resultsMovies = await accountIdData(1, mediaType, accountIdINP.value)
+
+        for (let pages = 1; pages <= resultsMovies.total_pages; pages++) {
+          resultsMovies = await accountIdData(pages, mediaType, accountIdINP.value)
+
+          for (let index = 0; index < resultsMovies.results.length; index++) {
+            const element = resultsMovies.results[index];
+            let timeDataReturn = await timerCalculator(mediaType, element.id)
+
+
+            let nameMedia = element.title
+
+            if (element.media_type == "tv") {
+              nameMedia = element.name
+
+            }
+            saveDisplay(element.poster_path, element.id, nameMedia, timeDataReturn, element.backdrop_path)
+            addTime(timeDataReturn)
+          } console.log(resultsMovies)
+        }
+        console.log(mediaType)
+        mediaType = "movies"
+
+      }
+      buttonAccountClick = false
+    }
+    else{
+      console.error('the input Id is empty')
+    }
+
+
+
+  })
+
+  creditsBTN.addEventListener('click', () => {
+    console.log('hello');
+    let creditsEl = document.querySelector('.credits-face');
+    displayNoneAndShow(creditsEl)
+
+  })
+  accountIdUser.addEventListener('click', () => {
+    console.log('hello');
+    let creditsEl = document.querySelector('.account-face-face');
+    displayNoneAndShow(creditsEl)
+
+  })
+  
+  function displayNoneAndShow(doc){
+    if (doc) {
+      if (doc.style.display === 'none' || doc.style.display === '') {
+        doc.style.display = 'flex';
+      } else {
+        doc.style.display = 'none';
+      }
+    } else {
+      console.error('Element with class "credits-face" not found.');
+    }
+
+  }
+
+  document.addEventListener('click', (event) => {
+    if (!event.target.closest('.search-container')) {
+      dropdown.style.display = 'none';
+
+    }
+  });
 
   function addTime(timeData) {
     //console.log(timeData)
@@ -163,23 +267,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function reduceTime(timeData) {
     totalHour -= timeData
+    if(totalHour < 0){totalHour = 0}
     updateTime(totalHour)
   }
 
   function updateTime(timeData) {
     
-    
+    if (totalHour == 0) {
+      counterTime.style.opacity = '0';
+      setTimeout(() => {
+        counterTime.classList.remove('show');
+        counterTime.style.display = 'none';
+      }, 500); // Match this duration to the CSS transition duration
+    } else {
+      counterTime.style.display = 'flex';
+      setTimeout(() => {
+        counterTime.classList.add('show');
+        counterTime.style.opacity = '1';
+      }, 10); // Small delay to allow display change to take effect before transition
+    }
+
     let timeResult = convertHourDaysMinutes(timeData)
     document.getElementById('daysTime').innerText = timeResult.days
     document.getElementById('hoursTime').innerText = timeResult.hours
     document.getElementById('minutesTime').innerText = timeResult.minutes
+
+    
     /*
     html elements below:
     <div class="detail-number">
                 <h1 id="dayMedia">days: 0</h1>
                 <h1 id="hourMedia">hours: 0</h1>
                 <h1 id="minutesMedia">minutes: 0</h1>
-
+  
                 
     </div>
     
@@ -197,7 +317,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (timeResult.days != days || timeResult.minutes != minute || timeResult.hours != hour) {
             console.log('is this working?')
             console.log(timeResult)
-
+  
           
             if(minute != timeResult.minutes){
               minute++
@@ -219,7 +339,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     */
 
-    
+
     /*
     counterTime.innerHTML = `
         <div class="detail-number">
@@ -250,70 +370,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     return { days, hours, minutes };
   }
-
-  async function accountIdData(page, mediaType) {
-    console.log(`/account/${accountId}/rated/${mediaType}?page=${page}&sort_by=created_at.asc`)
-    return await fetchDataApi(`/account/${accountId}/rated/${mediaType}?page=${page}&sort_by=created_at.asc`)
-
-  }
-
-  accountCalcBtn.addEventListener('click', async () => {
-    buttonAccountClick = true
-    let mediaType = "tv"
-
-    for (let i = 0; i < 2; i++) {
-
-      let resultsMovies = await accountIdData(1, mediaType)
-
-      for (let pages = 1; pages <= resultsMovies.total_pages; pages++) {
-        resultsMovies = await accountIdData(pages, mediaType)
-
-        for (let index = 0; index < resultsMovies.results.length; index++) {
-          const element = resultsMovies.results[index];
-          let timeDataReturn = await timerCalculator(mediaType, element.id)
-
-
-          let nameMedia = element.title
-
-          if (element.media_type == "tv") {
-            nameMedia = element.name
-
-          }
-          saveDisplay(element.poster_path, element.id, nameMedia, timeDataReturn, element.backdrop_path)
-          addTime(timeDataReturn)
-        } console.log(resultsMovies)
-      }
-      console.log(mediaType)
-      mediaType = "movies"
-      
-    }
-    buttonAccountClick = false
-
-
-
-  })
-
-  creditsBTN.addEventListener('click', () => {
-    console.log('hello');
-    let creditsEl = document.querySelector('.credits-face');
-    if (creditsEl) {
-      if (creditsEl.style.display === 'none' || creditsEl.style.display === '') {
-        creditsEl.style.display = 'flex';
-      } else {
-        creditsEl.style.display = 'none';
-      }
-    } else {
-      console.error('Element with class "credits-face" not found.');
-    }
-
-  })
-  document.addEventListener('click', (event) => {
-    if (!event.target.closest('.search-container')) {
-      dropdown.style.display = 'none';
-
-    }
-  });
-
 
 
 });
